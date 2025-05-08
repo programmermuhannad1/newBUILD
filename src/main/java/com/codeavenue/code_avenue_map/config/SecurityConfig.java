2c9a9f278e3c73bffda9b2bb292e78a2a0305030
programmermuhannad1 <oplkn99@gmail.com>
2025-05-09 00:19:22 +0300

    Initial commit to new repo

--- src/main/java/com/codeavenue/code_avenue_map/config/SecurityConfig.java
+++ src/main/java/com/codeavenue/code_avenue_map/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://test-liart-seven-41.vercel.app",
-                "https://newprojectfront.vercel.app"  // 👈 أضف هذا الرابط
+                "https://newprojectfront-arpe2aoxs-oplkn99-gmailcoms-projects.vercel.app"  // 👈 أضف هذا الرابط
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
